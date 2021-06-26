bc50f46f7eeacd843444d8b747d402447be79893
CCXXXI <ccxxxi@qq.com>
2021-06-27 04:00:47 +0800

    reformat by Black

--- code/models.py
+++ code/models.py
@@ -13,20 +13,20 @@ from utils import *
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-class VGG(nn.Module):
 
+class VGG(nn.Module):
     def __init__(self, features):
         super(VGG, self).__init__()
         self.features = features
         self.layer_name_mapping = {
-            '3': "relu1_2",
-            '8': "relu2_2",
-            '15': "relu3_3",
-            '22': "relu4_3"
+            "3": "relu1_2",
+            "8": "relu2_2",
+            "15": "relu3_3",
+            "22": "relu4_3",
         }
         for p in self.parameters():
             p.requires_grad = False
-    
+
     def forward(self, x):
         outs = []
         for name, module in self.features._modules.items():
@@ -39,20 +39,24 @@ class VGG(nn.Module):
 class MyConv2D(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size=3, stride=1):
         super(MyConv2D, self).__init__()
-        self.weight = torch.zeros((out_channels, in_channels, kernel_size, kernel_size)).to(device)
+        self.weight = torch.zeros(
+            (out_channels, in_channels, kernel_size, kernel_size)
+        ).to(device)
         self.bias = torch.zeros(out_channels).to(device)
-        
+
         self.in_channels = in_channels
         self.out_channels = out_channels
         self.kernel_size = (kernel_size, kernel_size)
         self.stride = (stride, stride)
-    
+
     def forward(self, x):
         return F.conv2d(x, self.weight, self.bias, self.stride)
-    
+
     def extra_repr(self):
-        s = ('{in_channels}, {out_channels}, kernel_size={kernel_size}'
-             ', stride={stride}')
+        s = (
+            "{in_channels}, {out_channels}, kernel_size={kernel_size}"
+            ", stride={stride}"
+        )
         return s.format(**self.__dict__)
 
 
@@ -60,19 +64,27 @@ class ResidualBlock(nn.Module):
     def __init__(self, channels):
         super(ResidualBlock, self).__init__()
         self.conv = nn.Sequential(
-            *ConvLayer(channels, channels, kernel_size=3, stride=1), 
-            *ConvLayer(channels, channels, kernel_size=3, stride=1, relu=False)
+            *ConvLayer(channels, channels, kernel_size=3, stride=1),
+            *ConvLayer(channels, channels, kernel_size=3, stride=1, relu=False),
         )
 
     def forward(self, x):
         return self.conv(x) + x
 
 
-def ConvLayer(in_channels, out_channels, kernel_size=3, stride=1, 
-    upsample=None, instance_norm=True, relu=True, trainable=False):
+def ConvLayer(
+    in_channels,
+    out_channels,
+    kernel_size=3,
+    stride=1,
+    upsample=None,
+    instance_norm=True,
+    relu=True,
+    trainable=False,
+):
     layers = []
     if upsample:
-        layers.append(nn.Upsample(mode='nearest', scale_factor=upsample))
+        layers.append(nn.Upsample(mode="nearest", scale_factor=upsample))
     layers.append(nn.ReflectionPad2d(kernel_size // 2))
     if trainable:
         layers.append(nn.Conv2d(in_channels, out_channels, kernel_size, stride))
@@ -91,50 +103,54 @@ class TransformNet(nn.Module):
         self.base = base
         self.weights = []
         self.downsampling = nn.Sequential(
-            *ConvLayer(3, base, kernel_size=9, trainable=True), 
-            *ConvLayer(base, base*2, kernel_size=3, stride=2), 
-            *ConvLayer(base*2, base*4, kernel_size=3, stride=2), 
+            *ConvLayer(3, base, kernel_size=9, trainable=True),
+            *ConvLayer(base, base * 2, kernel_size=3, stride=2),
+            *ConvLayer(base * 2, base * 4, kernel_size=3, stride=2),
         )
-        self.residuals = nn.Sequential(*[ResidualBlock(base*4) for i in range(5)])
+        self.residuals = nn.Sequential(*[ResidualBlock(base * 4) for i in range(5)])
         self.upsampling = nn.Sequential(
-            *ConvLayer(base*4, base*2, kernel_size=3, upsample=2),
-            *ConvLayer(base*2, base, kernel_size=3, upsample=2),
-            *ConvLayer(base, 3, kernel_size=9, instance_norm=False, relu=False, trainable=True),
+            *ConvLayer(base * 4, base * 2, kernel_size=3, upsample=2),
+            *ConvLayer(base * 2, base, kernel_size=3, upsample=2),
+            *ConvLayer(
+                base, 3, kernel_size=9, instance_norm=False, relu=False, trainable=True
+            ),
         )
         self.get_param_dict()
-    
+
     def forward(self, X):
         y = self.downsampling(X)
         y = self.residuals(y)
         y = self.upsampling(y)
         return y
-    
+
     def get_param_dict(self):
         """找出该网络所有 MyConv2D 层，计算它们需要的权值数量"""
         param_dict = defaultdict(int)
+
         def dfs(module, name):
             for name2, layer in module.named_children():
-                dfs(layer, '%s.%s' % (name, name2) if name != '' else name2)
+                dfs(layer, "%s.%s" % (name, name2) if name != "" else name2)
             if module.__class__ == MyConv2D:
                 param_dict[name] += int(np.prod(module.weight.shape))
                 param_dict[name] += int(np.prod(module.bias.shape))
-        dfs(self, '')
+
+        dfs(self, "")
         return param_dict
-    
+
     def set_my_attr(self, name, value):
         # 下面这个循环是一步步遍历类似 residuals.0.conv.1 的字符串，找到相应的权值
         target = self
-        for x in name.split('.'):
+        for x in name.split("."):
             if x.isnumeric():
                 target = target.__getitem__(int(x))
             else:
                 target = getattr(target, x)
-        
+
         # 设置对应的权值
         n_weight = np.prod(target.weight.shape)
         target.weight = value[:n_weight].view(target.weight.shape)
         target.bias = value[n_weight:].view(target.bias.shape)
-    
+
     def set_weights(self, weights, i=0):
         """输入权值字典，对该网络所有的 MyConv2D 层设置权值"""
         for name, param in weights.items():
@@ -145,16 +161,16 @@ class MetaNet(nn.Module):
     def __init__(self, param_dict):
         super(MetaNet, self).__init__()
         self.param_num = len(param_dict)
-        self.hidden = nn.Linear(1920, 128*self.param_num)
+        self.hidden = nn.Linear(1920, 128 * self.param_num)
         self.fc_dict = {}
         for i, (name, params) in enumerate(param_dict.items()):
             self.fc_dict[name] = i
-            setattr(self, 'fc{}'.format(i+1), nn.Linear(128, params))
-    
+            setattr(self, "fc{}".format(i + 1), nn.Linear(128, params))
+
     def forward(self, mean_std_features):
         hidden = F.relu(self.hidden(mean_std_features))
         filters = {}
         for name, i in self.fc_dict.items():
-            fc = getattr(self, 'fc{}'.format(i+1))
-            filters[name] = fc(hidden[:,i*128:(i+1)*128])
+            fc = getattr(self, "fc{}".format(i + 1))
+            filters[name] = fc(hidden[:, i * 128 : (i + 1) * 128])
         return filters
--- code/utils.py
+++ code/utils.py
@@ -13,24 +13,30 @@ import matplotlib.pyplot as plt
 
 cnn_normalization_mean = [0.485, 0.456, 0.406]
 cnn_normalization_std = [0.229, 0.224, 0.225]
-tensor_normalizer = transforms.Normalize(mean=cnn_normalization_mean, std=cnn_normalization_std)
+tensor_normalizer = transforms.Normalize(
+    mean=cnn_normalization_mean, std=cnn_normalization_std
+)
 epsilon = 1e-5
 
 
 def preprocess_image(image, target_width=None):
     """输入 PIL.Image 对象，输出标准化后的四维 tensor"""
     if target_width:
-        t = transforms.Compose([
-            transforms.Resize(target_width), 
-            transforms.CenterCrop(target_width), 
-            transforms.ToTensor(), 
-            tensor_normalizer, 
-        ])
+        t = transforms.Compose(
+            [
+                transforms.Resize(target_width),
+                transforms.CenterCrop(target_width),
+                transforms.ToTensor(),
+                tensor_normalizer,
+            ]
+        )
     else:
-        t = transforms.Compose([
-            transforms.ToTensor(), 
-            tensor_normalizer, 
-        ])
+        t = transforms.Compose(
+            [
+                transforms.ToTensor(),
+                tensor_normalizer,
+            ]
+        )
     return t(image).unsqueeze(0)
 
 
@@ -43,16 +49,17 @@ def image_to_tensor(image, target_width=None):
 
 def read_image(path, target_width=None):
     """输入图像路径，输出标准化后的四维 tensor"""
-    image = Image.open(path).convert('RGB')
+    image = Image.open(path).convert("RGB")
     return preprocess_image(image, target_width)
 
 
 def recover_image(tensor):
     """输入 GPU 上的四维 tensor，输出 0~255 范围的三维 numpy 矩阵，RGB 顺序"""
     image = tensor.detach().cpu().numpy()
-    image = image * np.array(cnn_normalization_std).reshape((1, 3, 1, 1)) + \
-    np.array(cnn_normalization_mean).reshape((1, 3, 1, 1))
-    return (image.transpose(0, 2, 3, 1) * 255.).clip(0, 255).astype(np.uint8)[0]
+    image = image * np.array(cnn_normalization_std).reshape((1, 3, 1, 1)) + np.array(
+        cnn_normalization_mean
+    ).reshape((1, 3, 1, 1))
+    return (image.transpose(0, 2, 3, 1) * 255.0).clip(0, 255).astype(np.uint8)[0]
 
 
 def recover_tensor(tensor):
@@ -66,7 +73,7 @@ def imshow(tensor, title=None, save=False):
     """输入 GPU 上的四维 tensor，然后绘制该图像"""
     image = recover_image(tensor)
     if save:
-        Image.fromarray(image).save('images/output.png')
+        Image.fromarray(image).save("images/output.png")
     print(image.shape)
     plt.imshow(image)
     if title is not None:
@@ -80,7 +87,9 @@ def mean_std(features):
         x = x.view(*x.shape[:2], -1)
         x = torch.cat([x.mean(-1), torch.sqrt(x.var(-1) + epsilon)], dim=-1)
         n = x.shape[0]
-        x2 = x.view(n, 2, -1).transpose(2, 1).contiguous().view(n, -1) # 【mean, ..., std, ...] to [mean, std, ...]
+        x2 = (
+            x.view(n, 2, -1).transpose(2, 1).contiguous().view(n, -1)
+        )  # 【mean, ..., std, ...] to [mean, std, ...]
         mean_std_features.append(x2)
     mean_std_features = torch.cat(mean_std_features, dim=-1)
     return mean_std_features
@@ -92,16 +101,16 @@ class Smooth:
         self.window_size = windowsize
         self.data = np.zeros((self.window_size, 1), dtype=np.float32)
         self.index = 0
-    
+
     def __iadd__(self, x):
         if self.index == 0:
             self.data[:] = x
         self.data[self.index % self.window_size] = x
         self.index += 1
         return self
-    
+
     def __float__(self):
         return float(self.data.mean())
-    
+
     def __format__(self, f):
         return self.__float__().__format__(f)
